b45c07b9bad29a1a83b777565f9943fb40057e3d
hykim <nagne11@gmail.com>
2018-07-20 21:46:29 +0900

    init

---
+++ cmake/Modules/exampleConfig.cmake
@@ -0,0 +1,30 @@
+INCLUDE(FindPkgConfig)
+PKG_CHECK_MODULES(PC_EXAMPLE example)
+
+FIND_PATH(
+    EXAMPLE_INCLUDE_DIRS
+    NAMES example/api.h
+    HINTS $ENV{EXAMPLE_DIR}/include
+        ${PC_EXAMPLE_INCLUDEDIR}
+    PATHS ${CMAKE_INSTALL_PREFIX}/include
+          /usr/local/include
+          /usr/include
+)
+
+FIND_LIBRARY(
+    EXAMPLE_LIBRARIES
+    NAMES gnuradio-example
+    HINTS $ENV{EXAMPLE_DIR}/lib
+        ${PC_EXAMPLE_LIBDIR}
+    PATHS ${CMAKE_INSTALL_PREFIX}/lib
+          ${CMAKE_INSTALL_PREFIX}/lib64
+          /usr/local/lib
+          /usr/local/lib64
+          /usr/lib
+          /usr/lib64
+)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(EXAMPLE DEFAULT_MSG EXAMPLE_LIBRARIES EXAMPLE_INCLUDE_DIRS)
+MARK_AS_ADVANCED(EXAMPLE_LIBRARIES EXAMPLE_INCLUDE_DIRS)
+
---
+++ swig/example_swig.i
@@ -0,0 +1,13 @@
+/* -*- c++ -*- */
+
+#define EXAMPLE_API
+
+%include "gnuradio.i"			// the common stuff
+
+//load generated python docstrings
+%include "example_swig_doc.i"
+
+%{
+%}
+
+
